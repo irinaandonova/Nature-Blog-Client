d6014b795bd5879390fcd4b4cfd6d30d1d8a1ab2
irinaandonova <irinaandonova97@gmail.com>
2023-01-05 10:12:34 +0200

    Add interface

---
+++ client/src/interfaces/HikingTrailInterface.tsx
@@ -0,0 +1,11 @@
+interface HikingTrailInterface {
+    id: number,
+    name: string, 
+    description: string,
+    imageUrl: string,
+    regionId: number,
+    hikingTrailDuration: number,
+    hikingDifficulty: number
+}
+
+export default HikingTrailInterface;
