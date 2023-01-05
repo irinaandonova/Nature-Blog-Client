fd11482943c6190faba85ab0d8f55c1e6a32d10a
irinaandonova <irinaandonova97@gmail.com>
2023-01-05 10:14:37 +0200

    Fetch region and modify  return value

--- client/src/components/DestinationCard/DestinationCard.tsx
+++ client/src/components/DestinationCard/DestinationCard.tsx
@@ -1,21 +1,30 @@
-import { FC } from "react";
-import DestinationProp from "../../interfaces/DestinationInterface";
+import { FC, useEffect, useState } from "react";
+import DestinationInterface from "../../interfaces/DestinationInterface";
+import RegionInterface from "../../interfaces/RegionInterface";
+import regionServices from "../../services/regionServices";
 
 interface DestinationProps {
-    destination: DestinationProp
+    destinationInfo: DestinationInterface
 }
 
-const DestinationCard: FC<DestinationProps> = ({ destination }) => {
+const DestinationCard: FC<DestinationProps> = ({ destinationInfo }) => {
+    const [region, setRegion] = useState<RegionInterface | null>(null);
+
+    useEffect(() => {
+        regionServices.getRegion(destinationInfo.regionId)
+            .then(result => setRegion(result))
+            .catch(err => console.log(err));
+
+    })
     return (
         <article className="destination-wrapper">
             <article className="image-article">
                 <img src="" alt="mountains" />
             </article>
             <article className="info-article">
-                <p className="info">{destination.name}</p>
-                <p className="info">{}</p>
-                <p className="info">{destination.description}</p>
-                <p className="info">Hiking Duration: </p>
+                <p className="info">Name: {destinationInfo.name}</p>
+                <p className="info">Region: {region?.name}</p>
+                <p className="info">{destinationInfo.description}</p>
             </article>
         </article>
     )
