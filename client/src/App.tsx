d42d1c994d6c023c309c5021d0afbd9304c32f3c
irinaandonova <irinaandonova97@gmail.com>
2023-01-06 11:17:12 +0200

    Add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Dashboard from './components/Dashboard/Dashboard';
-import Header from './components/Header/Header';
-import Details
-  from './components/Details/Details';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Header from './pages/Header/Header';
+import Details from './pages/Details/Details';
+import CreateDestination from './pages/CreateDestination/CreateDestination';
+
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element={<Dashboard filter={null}/>} />
+        <Route path='/' element={<Dashboard filter={null} />} />
         <Route path='/hiking-trails' element={<Dashboard filter={'hiking-trails'} />} />
         <Route path='/seasides' element={<Dashboard filter={'seasides'} />} />
         <Route path='/parks' element={<Dashboard filter={'parks'} />} />
         <Route path='/destination/:id' element={<Details />} />
+        <Route path='/destination/create' element={<CreateDestination />} />
       </Routes>
     </div>
   );
