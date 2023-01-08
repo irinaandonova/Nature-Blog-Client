2ab2ef62513da53de40f3c81759e34b11560c8e7
irinaandonova <irinaandonova97@gmail.com>
2023-01-08 15:06:34 +0200

    Add query client provider to app

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,10 +9,14 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const queryClient = new QueryClient();
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
     <App />
+    </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
