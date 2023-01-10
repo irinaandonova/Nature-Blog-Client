e67edd3fff9e41c63466fdb3fe027762001fea99
irinaandonova <irinaandonova97@gmail.com>
2023-01-10 19:52:59 +0200

    Modify paths

--- client/src/pages/Header/Header.jsx
+++ client/src/pages/Header/Header.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom"
-
+import './header.css'
 const Header = () => {
     return(
         <header>
             <nav>
-                <ul>
-                    <Link to="home">Home</Link>
-                    <Link to="hiking-trails">Hiking Trails</Link>
-                    <Link to="seasides">Seaside</Link>
-                    <Link to="parks">Parks</Link>
+                <ul className="header-links">
+                    <Link to="/" className="links">Home</Link>
+                    <Link to="/hiking-trails" className="links">Hiking Trails</Link>
+                    <Link to="/seasides" className="links">Seaside</Link>
+                    <Link to="/parks" className="links">Parks</Link>
+                    <Link to="/destination/create" className="links">Create Destination</Link>
                 </ul>
             </nav>
         </header>
