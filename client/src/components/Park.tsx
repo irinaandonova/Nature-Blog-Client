e50cc4edbf46a932bc0b1331fac344112084b5cb
irinaandonova <irinaandonova97@gmail.com>
2023-01-11 18:39:02 +0200

    Modify to mui

--- client/src/components/Park.tsx
+++ client/src/components/Park.tsx
@@ -1,14 +1,44 @@
+import { Box, RadioGroup, FormLabel, FormControlLabel, Radio } from '@mui/material';
+
 const Park = () => {
     return (
-        <article className="category-info">
-            <label htmlFor="hasPlayground">Does the park has playground?</label>
-            <input name="hasPlayground" value="Yes" type="radio"/>
-            <input name="hasPlayground" value="No" type="radio"/>
-            <label className="category-label" htmlFor="isDogFriendly">Are dogs allowed in this park?</label>
-            <input name="isDogFriendly" value="Yes" type="radio"/>
-            <input name="isDogFriendly" value="No" type="radio"/>
-        </article>
+        <Box>
+<FormLabel>Does the park has playground?</FormLabel>
+                <RadioGroup
+                    row
+                    aria-labelledby="demo-row-radio-buttons-group-label"
+                    name="row-radio-buttons-group"
+                >
+                    <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                    <FormControlLabel value="no" control={<Radio />} label="No" />
+                    <FormControlLabel
+                        value="disabled"
+                        disabled
+                        control={<Radio />}
+                        label="other"
+                    />
+                </RadioGroup>
+                
+                <RadioGroup
+                    row
+                    aria-labelledby="demo-row-radio-buttons-group-label"
+                    name="row-radio-buttons-group"
+                >
+                    <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                    <FormControlLabel value="no" control={<Radio />} label="No" />
+                    <FormControlLabel
+                        value="disabled"
+                        disabled
+                        control={<Radio />}
+                        label="other"
+                    />
+                </RadioGroup>
+        </Box>
+           
     );
 }
 
 export default Park;
+/* <label className="category-label" htmlFor="isDogFriendly">Are dogs allowed in this park?</label>
+            <input name="isDogFriendly" value="Yes" type="radio"/>
+            <input name="isDogFriendly" value="No" type="radio"/>*/
