7d69bb80a0e67c97580264a487c79a6116507ff0
irinaandonova <irinaandonova97@gmail.com>
2023-01-11 18:38:44 +0200

    Add event to form

--- client/src/components/HikingTrailInfo.tsx
+++ client/src/components/HikingTrailInfo.tsx
@@ -1,9 +1,21 @@
+import { useState, useContext } from 'react';
+import { DestinationContext } from '../context/destinationContext';
+
 const HikingTrail = () => {
+    const [duration, setDuration] = useState<number>(0);
+    const [difficulty, setDifficulty] = useState<number>(1);
+    
+    const { addHikingTrailInfo } = useContext(DestinationContext);
+    
+    
     return (
         <article className="category-info">
             <label className="category-label">Hiking Duration:</label>
-            <input name="hiking-duration" />
-            <select name="hiking-difficulty">
+            <input name="hiking-duration" onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
+                {
+                    addHikingTrailInfo({duration: Number(e.currentTarget.value), difficulty});
+                    setDuration(Number(e.currentTarget.value))}}/>
+            <select name="hiking-difficulty" onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setDifficulty(Number(e.currentTarget.value))}>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
