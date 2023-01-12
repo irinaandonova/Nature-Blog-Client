769f9c19e9beb25a18c4ee4e19b0b47a78ee0e16
irinaandonova <irinaandonova97@gmail.com>
2023-01-12 13:39:34 +0200

    Add interfaces

--- client/src/interfaces/CreateDestinationInterface.ts
+++ client/src/interfaces/CreateDestinationInterface.ts
@@ -1,1 +1,11 @@
-export default {}
+interface CreateDestinationInterface
+{
+    duration?: number ,
+    difficulty?: number,
+    isGuarded?: boolean,
+    offersUmbrella?: boolean,
+    isDogFriendly?: boolean,
+    hasPlayground?: boolean
+}
+
+export default CreateDestinationInterface;
---
+++ client/src/interfaces/FuncPropInterface.ts
@@ -0,0 +1,7 @@
+import CreateDestinationInterface from "./CreateDestinationInterface";
+
+interface FuncProp {
+    addInfo(info: CreateDestinationInterface): void
+}
+
+export default FuncProp;
