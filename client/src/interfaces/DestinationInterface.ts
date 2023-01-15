2f184f9060b922c6ee89b93afe3a7ad017244a21
irinaandonova <irinaandonova97@gmail.com>
2023-01-15 21:54:44 +0200

    Add interface

---
+++ client/src/interfaces/DestinationInterface.ts
@@ -0,0 +1,11 @@
+interface DestinationInterface {
+    id: number,
+    creatorId: number,
+    name: string, 
+    description: string,
+    imageUrl: string,
+    regionId: number,
+    type: string
+}
+
+export default DestinationInterface;
