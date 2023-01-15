4d56124c80e5040d5c0960840405a6e0fd5fee9c
irinaandonova <irinaandonova97@gmail.com>
2023-01-15 21:54:56 +0200

    Add pages

--- client/src/pages/Details/Details.tsx
+++ client/src/pages/Details/Details.tsx
@@ -1,20 +1,104 @@
-import { useState } from "react";
-import DestinationInterface from "../../interfaces/DestinationInterface";
+import { useContext } from "react";
+import DestinationFullInfoInterface from "../../interfaces/DestinationFullInfoIntreface";
+import { Card, CardMedia, CardContent, Typography, CardActions, Button, CircularProgress } from "@mui/material";
+import { AuthContext } from "../../auth/authContext";
+import { useQuery } from "react-query";
+import { useNavigate, useParams } from "react-router-dom";
+import axiosLocalInstance from "../../config/axiosConfig";
+import './details.css';
 
 const Details = ({ }) => {
-    const [destinationInfo, setDestinationInfo] = useState<DestinationInterface | null>(null);
-    /* <p className="info">Name: {destinationInfo.name}</p>
-                <p className="info">Region: {region?.name}</p>
-                <p className="info">{destinationInfo.description}</p>*/
-    return (
-        <section className="container">
-            <article className="image-article">
-                <img src="" alt="mountains" />
-            </article>
-            <article className="info-article">
+    const { id, type } = useParams();
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const getDestinationInfo = async () => {
+        const response = await axiosLocalInstance.get(`destinations/info/${id}`);
+        const data: DestinationFullInfoInterface = response.data;
+        
+        return data;
+    }
 
-            </article>
-        </section>
+    const onDeleteHandler = async () => {
+        const response = await axiosLocalInstance.delete(`destinations/${id}`, { data: { userId: user.id } });
+
+        if (response.status === 200)
+            navigate('/');
+    }
+    
+    
+    const { data, isError, isFetching, isLoading } = useQuery(['getDestinationFullInfoKey'], getDestinationInfo);
+    console.log(data?.type);
+
+    return (
+        <Card sx={{ maxWidth: 345 }} className="container">
+            {(isLoading || isFetching) ? <CircularProgress /> : null}
+            {isError ? <p>Something went wrong</p> : null}
+            <CardMedia
+                sx={{ height: 140 }}
+                image={data?.imageUrl}
+                title={data?.name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {data?.name}
+                </Typography>
+                <Typography gutterBottom variant="body2" component="div">
+                    {data?.description}
+                </Typography>
+                {
+                    data !== undefined && data.type === 'hikingTrail'
+                        ?
+                        <article>
+                            <Typography gutterBottom variant="body2" component="div">
+                                Hiking duration: {data?.hikingDuration}min
+                            </Typography>
+                            <Typography gutterBottom variant="body2" component="div">
+                                Hiking difficulty: {data?.difficulty}
+                            </Typography>
+                        </article>
+                        :
+                        null
+                }
+                {
+                    data !== undefined && data.type === 'seaside'
+                        ?
+                        <article>
+                            <Typography gutterBottom variant="body2" component="div">
+                                Guarded by life guard: {data.isGuarded === true ? 'Yes' : 'No'}
+                            </Typography>
+                            <Typography gutterBottom variant="body2" component="div">
+                                Offers umbrella: {data?.offersUmbrella == true ? 'Yes' : 'No'}
+                            </Typography>
+                        </article>
+                        :
+                        null
+                }
+                {
+                    data !== undefined && data.type === 'park'
+                        ?
+                        <article>
+                            <Typography gutterBottom variant="body2" component="div">
+                                Has playground: {data.isDogFriendly === true ? 'Yes' : 'No'}
+                            </Typography>
+                            <Typography gutterBottom variant="body2" component="div">
+                                Are dogs allowed: {data?.isDogFriendly == true ? 'Yes' : 'No'}
+                            </Typography>
+                        </article>
+                        :
+                        null
+                }
+                {
+                    user.id == data?.creatorId
+                        ?
+                        <CardActions>
+                            <Button size="small" onClick={onDeleteHandler}>Delete</Button>
+                            <Button size="small" onClick={() => navigate(`/destination/${id}/edit`)}>Edit</Button>
+                        </CardActions>
+                        :
+                        null
+                }
+            </CardContent>
+        </Card>
     );
 }
 
---
+++ client/src/pages/EditDestination/EditDestination.tsx
@@ -0,0 +1,178 @@
+import { Grid, TextField, FormControl, InputLabel, Select, MenuItem, Button, CircularProgress, Box } from "@mui/material";
+import { useContext, useState } from "react";
+import { useQuery } from "react-query";
+import { useNavigate, useParams } from "react-router-dom";
+
+import RegionInterface from "../../interfaces/RegionInterface";
+import axiosLocalInstance from "../../config/axiosConfig";
+import DestinationFullInfoInterface from "../../interfaces/DestinationFullInfoIntreface";
+import { AuthContext } from "../../auth/authContext";
+import CreateDestinationInterface from "../../interfaces/CreateDestinationInterface";
+import EditHikingTrail from "../../components/EditDestination/HikingTrail";
+import EditSeaside from "../../components/EditDestination/Seaside";
+import EditPark from "../../components/EditDestination/Park";
+
+const EditDestination = () => {
+    const initialState = { duration: undefined, difficulty: undefined, isGuarded: undefined, offersUmbrella: undefined, isDogFriendly: undefined, hasPlayground: undefined }
+    const { user } = useContext(AuthContext);
+    const { id } = useParams();
+    const [info, setInfo] = useState<CreateDestinationInterface>(initialState);
+    const navigate = useNavigate();
+
+    const toggleType = (type: string | undefined) => {
+        
+        if (type === 'hikingTrail')
+            return <EditHikingTrail addInfo={addInfo} destinationData={destinationData} />
+        else if (type === 'seaside')
+            return <EditSeaside addInfo={addInfo} destinationData={destinationData} />
+        else if (type === 'park')
+            return <EditPark addInfo={addInfo} destinationData={destinationData} />
+        else
+            return null;
+    }
+
+    const addInfo = (info: CreateDestinationInterface) => {
+        setInfo(info)
+    }
+
+    const getRegions = async () => {
+        const regions = await axiosLocalInstance.get('region');
+        const data: RegionInterface[] = regions.data;
+
+        return data;
+    }
+
+    const getDestinatinInfo = async () => {
+        const response = await axiosLocalInstance.get(`destinations/info/${id}`);
+        const data: DestinationFullInfoInterface = response.data;
+
+        return data;
+    }
+
+    const manageRegionOptions = (region: RegionInterface) => {
+        if (region.id === destinationData?.regionId)
+            return <MenuItem value={region.id} selected>{region.name}</MenuItem>;
+
+        return <MenuItem value={region.id}>{region.name}</MenuItem>;
+    }
+    const editDestinationHandler = async (name: string,
+        region: number,
+        description: string,
+        imageUrl: string,
+    ) => {
+        if (destinationData?.type == 'hikingTrail') {
+        console.log(user.id);
+            
+            const response = await axiosLocalInstance.put('destinations/hiking-trail/edit', {
+                id: destinationData.id,
+                name,
+                regionId: region,
+                userId: user.id,
+                description,
+                imageUrl,
+                duration: info.duration,
+                difficulty: info.difficulty,
+            });
+            console.log(response);
+            if (response.status == 200)
+                navigate('/');
+        }
+        else if (destinationData?.type === 'park') {
+            const response = await axiosLocalInstance.put('destinations/park/edit', {
+                name,
+                regionId: region,
+                userId: user.id,
+                description,
+                imageUrl,
+                isDogFriendly: info.isDogFriendly,
+                hasPlayground: info.hasPlayground,
+            });
+
+            if (response.status === 200)
+                navigate('/');
+        }
+        else if (destinationData?.type === 'seaside') {
+            const response = await axiosLocalInstance.put('destinations/seaside/edit', {
+                id: destinationData.id,
+                name,
+                regionId: region,
+                userId: user.id,
+                description,
+                imageUrl,
+                offersUmbrella: info.offersUmbrella,
+                isGuarded: info.isGuarded
+            });
+
+            if (response.status == 200)
+                navigate('/');
+        }
+    }
+
+    const { data: regionData, isLoading: regionLoadining, isFetching: regionFetching, isError: regionError } = useQuery(['getRegionsEditData'], getRegions);
+    const { data: destinationData, isLoading: destinationLoading, isFetching: destinationFetching, isError: destinationError } = useQuery(['getDestinationEditInfo'], getDestinatinInfo);
+
+    return (
+        <Box>
+            {destinationError || regionError ? <p>Something went wrong</p> : null}
+            {(regionLoadining || destinationLoading || destinationFetching || regionFetching)
+                ?
+                <CircularProgress />
+                :
+                <form onSubmit={(e: React.SyntheticEvent) => {
+                    e.preventDefault();
+                    console.log('submited');
+                    const target = e.target as typeof e.target & {
+                        name: { value: string }
+                        region: { value: number }
+                        description: { value: string }
+                        imageUrl: { value: string },
+                    };
+                    const name = target.name.value;
+                    const region = target.region.value;
+                    const description = target.description.value;
+                    const imageUrl = target.imageUrl.value;
+
+                    editDestinationHandler(name, region, description, imageUrl);
+                }}>
+                    <Grid container direction="column" className="container">
+                        <Grid item>
+                            <TextField label="Destination Name" name="name" margin="normal" type="text" inputProps={{ minLength: 2, maxLength: 100 }} defaultValue={destinationData?.name} />
+                        </Grid>
+                        <Grid item>
+                            <FormControl margin="normal">
+                                <InputLabel htmlFor="region" >Region: </InputLabel>
+                                <Select
+                                    name="region"
+                                    autoWidth
+                                    label="region"
+                                    className="region-select"
+                                    margin="dense"
+                                >
+                                    {
+                                        regionData
+                                            ?
+                                            regionData.map(r => manageRegionOptions(r))
+                                            :
+                                            null
+                                    }
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item>
+                            <TextField type="text" sx={{ width: 222 }} multiline={true} rows={10} label="Description" name="description" margin="normal" inputProps={{ minLength: 2, maxLength: 960, }} defaultValue={destinationData?.description} />
+                        </Grid>
+                        <Grid item>
+                            <FormControl>
+                                <TextField type="text" label="Image path" name="imageUrl" margin="normal" inputProps={{ minLength: 2, maxLength: 960 }} defaultValue={destinationData?.imageUrl} />
+                            </FormControl>
+                        </Grid>
+                        {toggleType(destinationData?.type)}
+                        <Button type="submit" variant="contained" className="submit-btn">Edit</Button>
+                    </Grid>
+                </form>
+            }
+        </Box >
+    );
+}
+
+export default EditDestination;
