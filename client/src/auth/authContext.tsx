e72464e77ccebe70721e5f6171509a8c56d8dc26
irinaandonova <irinaandonova97@gmail.com>
2023-01-18 14:03:22 +0200

    Modify username

--- client/src/auth/authContext.tsx
+++ client/src/auth/authContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, ReactNode, useState } from "react";
 import UserInterface from "../interfaces/UserInterface";
 
-const initialValue: UserInterface = { id: 3, username: 'irina', email: 'irinaandonova97@gmail.com' };
+const initialValue: UserInterface = { id: 1, username: 'irina1', email: 'irinaandonova97@gmail.com' };
 export const AuthContext = createContext({ user: initialValue });
 
 export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
